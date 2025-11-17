8883e0b5d854d8046f8d2ed4a119ac3eaf979a5e
alessandro <alessandro.leanza@supsi.ch>
2025-11-17 11:46:58 +0100

    Removed some old nodes from setup.py

--- exo_control/setup.py
+++ exo_control/setup.py
@@ -15,21 +15,15 @@ setup(
     entry_points={
         'console_scripts': [
             'twoboards_odrive = exo_control.twoboards_odrive:main',
-            'admittance_controller = exo_control.admittance_controller:main',
+            'admittancecontrol_box = exo_control.admittancecontrol_box:main',
             'stoop_trajs = exo_control.stoop_trajs:main',
             'yolo_grasp_detector = exo_control.yolo_grasp_detector:main',
-            'motors_control = exo_control.motors_control:main',
-            'single_admittance_controller = exo_control.single_admittance_controller:main',
             'nominal_traj = exo_control.nominal_traj:main',
             'traj_client = exo_control.traj_client:main',
             'glasses_yolo_detector = exo_control.glasses_yolo_detector:main',
             'rel_no_vision_trigger = exo_control.rel_no_vision_trigger:main',
             'ass_no_vision_trigger = exo_control.ass_no_vision_trigger:main',
-            'admittance_controller_assistance = exo_control.admittance_controller_assistance:main',
             'realsense_yolo = exo_control.realsense_yolo:main',
-            'admittance_controller_perception = exo_control.admittance_controller_perception:main',
-            'admittance_controller_perception_offset = exo_control.admittance_controller_perception_offset:main',
-            'admittancecontrol_box = exo_control.admittancecontrol_box:main',
         ],
     },
 )
